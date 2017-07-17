dad2f14860f581c18f7cd91bd197b43adae7f282
thebhaiwas <thebhaiwas@gmail.com>
2017-07-17 11:36:45 +0530

    Activities added

---
+++ app/src/main/java/hr/smallcouncil/feedbackapp/WriteFeedback.java
@@ -0,0 +1,13 @@
+package hr.smallcouncil.feedbackapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WriteFeedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_feedback);
+    }
+}
